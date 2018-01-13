d2d722ff8a47a30d918a3a5148cf3fd95a5e52d9
ZizouImrane <imrane60100@hotmail.fr>
2018-01-13 21:58:54 +0100

    le menu

--- membre/membre/menu.php
+++ membre/membre/menu.php
@@ -1,15 +1,15 @@
 <link rel="stylesheet" type="text/css" href="menu.css">
 <?php
 echo ' 
-	<div id="menumembre" align="left">
+		<div id="bonjour">Bonjour '.Membre::info($_SESSION['id'], 'pseudo').'.'.'
+	</div>'
+?>
+	<div id="menumembre">
 		<ul id=menubarre>
-			<li><a href="index.php" class="bouton">Accueil</a></li>
-			<li><a href="messagerie.php" class="bouton">Messagerie</a></li>
-			<li><a href="profil.php" class="bouton">Profil</a></li>
+			<li><a href="index.php" >Accueil</a></li>
+			<li><a href="messagerie.php" >Messagerie</a></li>
+			<li><a href="profil.php">Profil</a></li>
 		</ul>
 	</div>
 	
-	<h1>Bonjour '.Membre::info($_SESSION['id'], 'pseudo').'</h1>
-	<hr width="100%"  color="2a95be"/>
-	<br />';
-?>
+	<br>
